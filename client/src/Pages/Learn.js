def2ee30141c331df65ce7b80e6ea87bb22ac0a8
Sharvai <sharvai101@gmail.com>
2023-12-26 23:42:55 -0500

    added setProject

--- client/src/Pages/Learn.js
+++ client/src/Pages/Learn.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import api from '../axiosConfig';
+import Loading from '../Components/Loading';
 
 const Learn = () => {
   const location = useLocation();
@@ -9,6 +10,7 @@ const Learn = () => {
   const projectID = queryParams.get('projectID');
 
   const [project, setProject] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     api.post('/get_project_details', { "projectID": projectID }, {
@@ -17,7 +19,9 @@ const Learn = () => {
       }
     })
     .then(response => {
-      console.log(response);
+      setProject(response.data);
+      console.log(project);
+      setIsLoading(false);
     })
     .catch(error => {
       console.error("Error fetching data:", error);
@@ -27,17 +31,19 @@ const Learn = () => {
   return (
     <div>
       <Navbar />
-      
-      <div className='px-4 lg:px-20 pt-5'>
-        <div className='grid grid-cols-3 gap-4'>
-          <div className='col-span-2 shadow-lg rounded-lg'>
-            hi
-          </div>
-          <div className='col-span-1 shadow-lg rounded-lg p-2'>
-            <audio src={{}} controls></audio>
+      { isLoading && <Loading /> }
+      { !isLoading && 
+        <div className='px-4 lg:px-20 pt-5'>
+          <div className='grid grid-cols-3 gap-4'>
+            <div className='col-span-2 shadow-lg rounded-lg'>
+              hi
+            </div>
+            <div className='col-span-1 shadow-lg rounded-lg p-2'>
+              <audio src={project.audioFilePath} controls></audio>
+            </div>
           </div>
         </div>
-      </div>
+      }
     </div>
   );
 };
