2d3155bd49e4d7112912b82829e46b1849b5bfda
Sharvai <sharvai101@gmail.com>
2023-12-28 19:40:20 -0500

    create type and talk modes

--- client/src/Components/DoubtModal.js
+++ client/src/Components/DoubtModal.js
@@ -11,6 +11,8 @@ function DoubtModal({ audioRef, toggleModal, project }) {
 
     const [currentMessage, setCurrentMessage] = useState("");
 
+    const [isTypeMode, setIsTypeMode] = useState(true);
+
     const handleSubmit = () => {
         const trimmedMessage = currentMessage.trim();
         if (trimmedMessage !== "") {
@@ -36,13 +38,33 @@ function DoubtModal({ audioRef, toggleModal, project }) {
         }
     };
 
+    const handleOptionClick = (option) => {
+        console.log(option);
+        if (option == "type" && !isTypeMode) {
+            setIsTypeMode(true);
+        }
+        if (option == "talk" && isTypeMode) {
+            setIsTypeMode(false);
+        }
+    };
+
     return (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
             {/* Modal Content */}
             <div className="relative top-20 mx-auto p-5 border w-4/5 md:w-1/2 shadow-lg rounded-md bg-white">
                 <div className='grid grid-cols-1 w-full'>
                     <div className='grid grid-cols-2'>
-                        <p style={{fontSize: 20}}>Ask a doubt!</p>
+                        <div className='flex'>
+                            <p style={{fontSize: 20}}>Ask a doubt!</p>
+                            <div className='ml-3 rounded-l-full rounded-r-full bg-gray-200 p-1 grid grid-cols-2' style={{width: 150}}>
+                                <div onClick={() => handleOptionClick("type")} className={`rounded-l-full rounded-r-full ${isTypeMode == true?'bg-green-700' : 'bg-gray-200'} px-2 grid grid-cols-1 items-center cursor-pointer`}>
+                                    <p className={`${isTypeMode == true?'text-white' : 'text-black'} text-center`} style={{fontSize: 12}}>Type</p>
+                                </div>
+                                <div onClick={() => handleOptionClick("talk")} className={`rounded-l-full rounded-r-full ${isTypeMode == false?'bg-green-700' : 'bg-gray-200'} px-2 grid grid-cols-1 items-center cursor-pointer`}>
+                                    <p className={`${isTypeMode == false?'text-white' : 'text-black'} text-center`} style={{fontSize: 12}}>Talk</p>
+                                </div>
+                            </div>
+                        </div>
                         <button className='justify-self-end' onClick={toggleModal}>Close</button>
                     </div>
                     <div className='border rounded border-gray-300 p-4 mb-2 mt-4 overflow-auto grid grid-cols-1' style={{maxHeight: 600}}>
@@ -52,22 +74,25 @@ function DoubtModal({ audioRef, toggleModal, project }) {
                             <UserTextBlob key={index} message={message} />
                         ))}
                     </div>
-                    <div className="flex items-center">
-                        <textarea
-                            className="w-full resize-none border rounded border-gray-300 p-2"
-                            rows="2"
-                            placeholder="Enter your question here"
-                            value={currentMessage}
-                            onChange={(e) => setCurrentMessage(e.target.value)}
-                        />
-                        { isLoading && <div className='ml-2'><Loading /></div>}
-                        { !isLoading && <button
-                            className="bg-green-700 text-white font-bold py-2 px-4 rounded ml-4"
-                            onClick={handleSubmit}
-                        >
-                            Ask!
-                        </button> }
-                    </div>
+                    { isTypeMode &&
+                        <div className="flex items-center">
+                            <textarea
+                                className="w-full resize-none border rounded border-gray-300 p-2"
+                                rows="2"
+                                placeholder="Enter your question here"
+                                value={currentMessage}
+                                onChange={(e) => setCurrentMessage(e.target.value)}
+                            />
+                            { isLoading && <div className='ml-2'><Loading /></div>}
+                            { !isLoading && <button
+                                className="bg-green-700 text-white font-bold py-2 px-4 rounded ml-4"
+                                onClick={handleSubmit}
+                            >
+                                Ask!
+                            </button> }
+                        </div>
+                    }
+                    { !isTypeMode && <div>Talk mode</div> }
                 </div>
             </div>
         </div>
