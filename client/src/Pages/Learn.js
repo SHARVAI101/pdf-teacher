64c09543eb24683eba0ca7885971fc4b8ec32b98
Sharvai <sharvai101@gmail.com>
2023-12-29 02:33:47 -0500

    created notes feature

--- client/src/Pages/Learn.js
+++ client/src/Pages/Learn.js
@@ -6,6 +6,7 @@ import Loading from '../Components/Loading';
 import PdfViewer from '../Components/PdfViewer';
 import AudioPlayer from '../Components/AudioPlayer';
 import QuizModal from '../Components/QuizModal';
+import NotesModal from '../Components/NotesModal';
 
 const Learn = () => {
   const location = useLocation();
@@ -15,9 +16,14 @@ const Learn = () => {
   const [project, setProject] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isNotesModalOpen, setIsNotesModalOpen] = useState(false);
 
   const toggleModal = () => {    
     setIsModalOpen(!isModalOpen);
+  };
+
+  const toggleNotesModal = () => {    
+    setIsNotesModalOpen(!isNotesModalOpen);
   };
 
   useEffect(() => {
@@ -40,6 +46,7 @@ const Learn = () => {
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
   const [toolTipX, setToolTipX] = useState(0.0);
   const [toolTipY, setToolTipY] = useState(0.0);
+  const [isToolTipTextSave, setIsToolTipTextSave] = useState(true);
 
   const textRef = useRef(null);
 
@@ -47,10 +54,11 @@ const Learn = () => {
     const selection = window.getSelection();
     if (!selection.rangeCount || selection.toString().trim() === '') {
       setIsTooltipVisible(false);
+      setIsToolTipTextSave(true);
       return;
     }
-    console.log(selection.getRangeAt(0).getBoundingClientRect());
-    console.log(selection.getRangeAt(0).getBoundingClientRect()["x"]);
+    // console.log(selection.getRangeAt(0).getBoundingClientRect());
+    // console.log(selection.getRangeAt(0).getBoundingClientRect()["x"]);
     var x = selection.getRangeAt(0).getBoundingClientRect()["x"];
     var y = selection.getRangeAt(0).getBoundingClientRect()["y"];
     var width = selection.getRangeAt(0).getBoundingClientRect()["width"];
@@ -59,12 +67,27 @@ const Learn = () => {
     setIsTooltipVisible(true);
   };
 
-  const copyToClipboard = () => {
+  const saveToNotes = () => {
     const selection = window.getSelection();
-    navigator.clipboard.writeText(selection.toString()).then(() => {
-        // Possibly show a confirmation message
-        setIsTooltipVisible(false);
+    setIsToolTipTextSave(false);
+    setToolTipX(toolTipX+20);
+    var note = selection.toString();
+    api.post('/save_to_notes', { "projectID": projectID, "note": note }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
     });
+
+    // navigator.clipboard.writeText(selection.toString()).then(() => {
+        // Possibly show a confirmation message
+        // setIsTooltipVisible(false);
+    // });
   };
 
   
@@ -95,7 +118,7 @@ const Learn = () => {
                       <p id="explanation-p" className='explanation-p' style={{fontSize: 25}}>Explanation</p>
                     </div>
                     <div className='justify-self-end'>
-                      <button><img src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png" className="m-1 mr-6" style={{width: 30}} alt="" /></button>
+                      <button onClick={toggleNotesModal}><img src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png" className="m-1 mr-6" style={{width: 30}} alt="" /></button>
                       <button><img src="https://cdn-icons-png.flaticon.com/512/2793/2793765.png" className="m-1 mr-4" style={{width: 30}} alt="" /></button>
                     </div>
                   </div>
@@ -105,7 +128,10 @@ const Learn = () => {
                   <p className='p-2' ref={textRef} >{project.explanation}</p>
                   {isTooltipVisible && (
                     <div className="absolute bg-black text-white p-2 rounded-md" style={{ top: toolTipY, left: toolTipX }}>
-                      <button onClick={copyToClipboard} className='flex items-center'>Save to Notes <img src="https://cdn-user-icons.flaticon.com/22664/22664815/1703816755490.svg?token=exp=1703817659~hmac=e3b59c23f7511dbeec261b53dab3bc82" className="ml-2" style={{width: 18}} alt="" /></button>
+                      <button onClick={saveToNotes} className='flex items-center'>
+                        {isToolTipTextSave && <span className='flex'>Save to Notes <img src="https://cdn-user-icons.flaticon.com/22664/22664815/1703816755490.svg?token=exp=1703817659~hmac=e3b59c23f7511dbeec261b53dab3bc82" className="ml-2" style={{width: 18}} alt="" /></span>}
+                        {!isToolTipTextSave && <span className='flex'><img src="https://cdn-icons-png.flaticon.com/512/1443/1443000.png " className="mr-2" style={{width: 25}} alt="" /> Added!</span>}
+                      </button>
                     </div>
                   )}
                 </div>
@@ -117,6 +143,9 @@ const Learn = () => {
         { isModalOpen && (
           <QuizModal toggleModal={toggleModal} project={project}/>
         )}
+        {
+          isNotesModalOpen && <NotesModal toggleNotesModal={toggleNotesModal} projectID={projectID} />
+        }
       </div>      
     </div>
   );
